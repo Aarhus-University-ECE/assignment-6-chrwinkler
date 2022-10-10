664a227c0de4d4accbbd9df2de2443d274d13b31
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-10-10 06:50:06 +0200

    Add files via upload

---
+++ max.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+int max(int *numbers, int size)
+{
+    // Excercise 2
+    // Implement your code below...
+
+    // Preconditions
+    // it has to be array of unkown size and values
+    // then find the maxiumum value in the array.
+    // if it 0 or less, assert.
+
+    // finding maximum value
+    for (int i = 0; i < size; i++)
+    {
+        if (*numbers < *(numbers + i))
+        {
+            *numbers = *(numbers + i);
+        }
+    }
+    assert(size < 0);
+
+    return -1;
+}
